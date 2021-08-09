4b461bb769ae00af857b45153845c9e6dc029cd1
soumyadip201 <soumyadip201@gmail.com>
2021-08-09 14:54:37 +0530

    created the home.ejs

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,3 +1,9 @@
 module.exports.home = function (req, res) {
-    return res.end('<h1>Express is up for ConnecTi</h1>')
+    //return res.end('<h1>Express is up for ConnecTi</h1>');
+
+    return res.render('home', {
+        title: "Home"
+    });
 }
+
+//module.exports.actionName = function (req, res) {}
