66dd9427d8356e128e487fb5a78ad0815f59fcc3
soumyadip201 <soumyadip201@gmail.com>
2021-08-09 14:43:37 +0530

    set up the view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 //use express routers
 app.use('/', require('./routes'));
 
+//set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function (err) {
     if (err) { console.log(`Error in running the server : ${err}`); }
     console.log(`Server is up and running on port: ${port}`);
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ const homeController = require('../controller/home_controller');
 console.log('router loaded....');
 
 router.get('/', homeController.home);       //for simple => default =>goes to home_controller in controller file
-router.use('/user', require('./users'));   //for any request =>   /users/(any_filee)  =>got to users.js and then from that router to controller file
+router.use('/user', require('./users'));   //for any request =>   /users/(any_filee)  =>got to users.js and then from that router to controller file 
+//this user contains all the further request under /users like user/like , user/profile, user/comments etc
 
 
 //for any further routes,access from here
