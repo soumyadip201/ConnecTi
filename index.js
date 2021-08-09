de72c1939cd1f96e8c85e703370aa4f7f9a52853
soumyadip201 <soumyadip201@gmail.com>
2021-08-09 14:33:54 +0530

    add a controller and router for users

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 
 //use express routers
-app.use('/', require('./routes/index'));
+app.use('/', require('./routes'));
 
 app.listen(port, function (err) {
     if (err) { console.log(`Error in running the server : ${err}`); }
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ const homeController = require('../controller/home_controller');
 
 console.log('router loaded....');
 
-router.get('/', homeController.home);
+router.get('/', homeController.home);       //for simple => default =>goes to home_controller in controller file
+router.use('/user', require('./users'));   //for any request =>   /users/(any_filee)  =>got to users.js and then from that router to controller file
+
+
+//for any further routes,access from here
+//router.use('/routerName',require('/routerfile'));
 
 module.exports = router;
