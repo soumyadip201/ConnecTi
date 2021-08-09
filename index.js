6a8c9775223e8b9158612ac769141061728a54e6
soumyadip201 <soumyadip201@gmail.com>
2021-08-09 20:36:46 +0530

    including the assets file and doing the functionalities

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const app = express();
 const port = 8000;
 const expressLayout = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
 //we have write before route as in route those views are going to be rendered 
 //so before that we have to tell that all the views that will be rendered belong to some sort of a layout or a function what layout is
 app.use(expressLayout);
+
+//extract style and scripts from sub-pages into the layout.ejs
+app.set('layout extractStyles', true); //for css
+app.set('layout extractScripts', true); //for js
+
 
 //use express routers
 app.use('/', require('./routes'));
