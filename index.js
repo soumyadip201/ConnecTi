84440a5a2f09eed293c5a3511aec77e6247bfc7c
soumyadip201 <soumyadip201@gmail.com>
2021-08-09 20:12:24 +0530

    included partials and layouts

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+//Layouts => npm install express-ejs-layouts 
+
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayout = require('express-ejs-layouts');
+
+//we have write before route as in route those views are going to be rendered 
+//so before that we have to tell that all the views that will be rendered belong to some sort of a layout or a function what layout is
+app.use(expressLayout);
 
 //use express routers
 app.use('/', require('./routes'));
