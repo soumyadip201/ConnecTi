0bca3bbc7652a5c5169ca63ef975c5868077715b
soumyadip201 <soumyadip201@gmail.com>
2021-08-17 12:59:10 +0530

    added controllers,routers for signin and signup

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -6,3 +6,25 @@ module.exports.profile = function (req, res) {
     })
     //return res.end('<h1>User Profile</h1>');
 }
+
+//rendered the signup page
+module.exports.signup = function (req, res) {
+    return res.render('user_signup', {
+        title: "ConnecTi | Sign up"
+    })
+}
+
+
+//rendered the signin page
+module.exports.signin = function (req, res) {
+    return res.render('user_signin', {
+        title: "ConnecTi | Sign in"
+    })
+}
+
+//get the signup data
+module.exports.create = function (req, res) { }
+
+//sign in and create a session for user
+module.exports.createSession = function (req, res) { }
+
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ const usersController = require('../controller/user_controller');
 
 router.get('/profile', usersController.profile);
 
+router.get('/sign-up', usersController.signup);
+
+router.get('/sign-in', usersController.signin);
+
 module.exports = router;
