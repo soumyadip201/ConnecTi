27348d7d58e55e30d087a506c64554e51a5e1cd4
soumyadip201 <soumyadip201@gmail.com>
2021-08-18 10:09:31 +0530

    manual-authentication

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -50,5 +50,34 @@ module.exports.create = function (req, res) {
 //sign in and create a session for user
 module.exports.createSession = function (req, res) {
 
+
+    //find the user
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) { console.log("Error in signup"); return; }
+
+        //handle user found
+        if (user) {
+
+            //handle password which dont match
+            if (user.password !== req.body.password) {
+                return res.redirect('back');
+            }
+
+            //handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/user/profile');
+
+        } else {
+
+            //handle user not found
+            return res.redirect('back')
+        }
+
+    });
+
+
+
+
+
 }
 
