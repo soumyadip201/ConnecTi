c726a398341caebf84590863f7030cdf1be203e6
soumyadip201 <soumyadip201@gmail.com>
2021-08-24 21:38:50 +0530

    posts updated at database

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ console.log('router loaded....');
 router.get('/', homeController.home);       //for simple => default =>goes to home_controller in controller file
 router.use('/user', require('./users'));   //for any request =>   /users/(any_filee)  =>got to users.js and then from that router to controller file 
 //this user contains all the further request under /users like user/like , user/profile, user/comments etc
-
+router.use('/posts', require('./posts'));
 
 //for any further routes,access from here
 //router.use('/routerName',require('/routerfile'));
